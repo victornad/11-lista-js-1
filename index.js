45fa30a5bc12b4505bb22d68e83f2a51e6cef8fe
victornad <victor.nadler.10@gmail.com>
2025-10-30 18:59:53 -0300

    Corrige as funções de cálculo no index.js

--- index.js
+++ index.js
@@ -1,13 +1,49 @@
-/**** Escreva o código abaixo 👇******/
 
+function somar(a, b) {
+  return a + b;
+}
 
+function multiplicar(a, b) {
+  return a * b;
+}
 
+function subtrair(a, b) {
+  return a - b;
+}
 
+function dividir(a, b) {
+  if (b === 0) {
+    throw new Error("Erro: divisão por zero");
+  } else {
+    return a / b;
+  }
+}
 
+function calcularMedia(n1, n2, n3, n4) {
+  const soma = n1 + n2 + n3 + n4;
+  return dividir(soma, 4);
+}
 
+function elevarPotencia(base, expoente) {
+  return Math.pow(base, expoente);
+}
 
+function calcularAreaRetangulo(base, altura) {
+  return base * altura;
+}
 
-/**** Escreva o código acima ☝️******/
+function verificarParOuImpar(numero) {
+  return numero % 2 === 0 ? "par" : "ímpar"; 
+}
+
+function calcularRaizQuadrada(numero) {
+  return Math.sqrt(numero);
+}
+
+function calcularHipotenusa(cateto1, cateto2) {
+  return Math.sqrt(cateto1 ** 2 + cateto2 ** 2);
+}
+
 
 //Não altere as linhas abaixo
 module.exports = {
